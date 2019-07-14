fc85cbe2b9bea814f3c0f60eed093702408b984e
Amrit Gill <amrit.gill@coveros.com>
2019-07-14 09:45:33 -0400

    Update rds.go

--- pkg/rdsLib/rds.go
+++ pkg/rdsLib/rds.go
@@ -77,7 +77,7 @@ func Crud(rdsObject RDS, actionType RDSAction, crStatusCreated bool, client clie
 			return err
 		}
 
-	// restore from snapshot
+	// restore ( means different for each object )
 	case RESTORE:
 
 		if !crStatusCreated {
